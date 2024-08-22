42d587bfffb091b1d1176121026075f68e795fdd
boylett <ryan@boylett.dev>
2024-08-22 16:30:51 +0100

    updated languages

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
-import './App.css';
-import Block from './components/Block';
-import Logo from './components/Logo';
+import "./App.css";
+import Block from "./components/Block";
+import Logo from "./components/Logo";
 
-function App () {
+function App() {
   const content = [
     {
       title: {
-        prefix: 'I speak',
+        prefix: "I speak",
         words: [
-          'Apache',
-          'Bash',
-          'CoffeeScript',
-          'CSS',
-          'HTML',
-          'JavaScript',
-          'Laravel',
-          'MongoDB',
-          'MySQL',
-          'Next.js',
-          'Nginx',
-          'Node.js',
-          'PHP',
-          'PostgreSQL',
-          'Python',
-          'React',
-          'Remix',
-          'SASS',
-          'SCSS',
-          'Slim',
-          'Stylus',
-          'Symfony',
-          'TypeScript',
+          "Apache",
+          "Bash",
+          "CoffeeScript",
+          "CSS",
+          "HTML",
+          "JavaScript",
+          "Laravel",
+          "MongoDB",
+          "Mongoose",
+          "MySQL",
+          "Next.js",
+          "Nginx",
+          "Node.js",
+          "PHP",
+          "PostgreSQL",
+          "Python",
+          "React",
+          "Remix",
+          "SASS",
+          "SCSS",
+          "Slim",
+          "Stylus",
+          "Symfony",
+          "Svelte",
+          "TypeScript",
+          "Vite",
         ],
       },
       content: [
-        'As a full stack developer, I make ample use of the web\'s latest and most robust technologies - React, TypeScript, PHP, HTML, CSS and, of course, JavaScript.',
-        'I\'ve written countless React components, jQuery extensions, WordPress plugins, Magento components, complex database queries and various scriptlets for use within larger scale projects, and am actively learning Node.js and Laravel.',
+        "As a full stack developer, I make ample use of the web's latest and most robust technologies - React, Svelte, TypeScript, PHP, HTML, CSS and, of course, JavaScript.",
+        "I've written countless React and Svelte components, jQuery extensions, WordPress plugins, Magento components, complex database queries and various scriptlets for use within larger scale projects, and am actively learning more.",
       ],
       thumbnail: false,
       links: [
         {
-          href: 'https://github.com/boylett#',
-          icon: 'fab fa-github-alt',
-          text: 'github.com/boylett'
+          href: "https://github.com/boylett#",
+          icon: "fab fa-github-alt",
+          text: "github.com/boylett",
         },
         {
-          href: 'https://linkedin.com/in/boylett',
-          icon: 'fab fa-linkedin',
-          text: 'linkedin.com/in/boylett'
+          href: "https://linkedin.com/in/boylett",
+          icon: "fab fa-linkedin",
+          text: "linkedin.com/in/boylett",
         },
       ],
     },
@@ -59,7 +62,15 @@ function App () {
         <Logo />
       </header>
       <section className="app__content">
-        { content.map((blockContent, index) => <Block title={ blockContent.title } content={ blockContent.content } thumbnail={ blockContent.thumbnail } links={ blockContent.links } key={ index } />) }
+        {content.map((blockContent, index) => (
+          <Block
+            title={blockContent.title}
+            content={blockContent.content}
+            thumbnail={blockContent.thumbnail}
+            links={blockContent.links}
+            key={index}
+          />
+        ))}
       </section>
     </main>
   );
